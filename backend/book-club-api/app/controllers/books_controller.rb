540514728d17b0a0922a9eb9b7cca175ceef4b48
Ella <michaella.latkovic@gmail.com>
2019-10-16 19:19:28 -0700

    added basic CRUD

--- backend/book-club-api/app/controllers/books_controller.rb
+++ backend/book-club-api/app/controllers/books_controller.rb
@@ -1,2 +1,40 @@
 class BooksController < ApplicationController
+  before_action :set_params, only: [:show, :update, :destroy]
+
+  def index
+    @books = Book.all
+    render json: @books, only: [:id, :title, :author, :image], status: 200
+  end
+
+  def create
+    @book = Book.create(book_params)
+    render json: @book, status: 200
+  end
+
+  def show
+    render json: @book, status: 200
+  end
+
+  def update
+    @book.update(book_params)
+    if @book.save
+      render json: @book, status: 200
+    else
+      render json: { errors: @book.errors.full_messages }, status: 422
+    end
+  end
+
+  def destroy
+    @book.destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :image)
+  end
+
+  def set_params
+    @book = Book.find(params[:id])
+  end
 end
--- backend/book-club-api/app/controllers/reviews_controller.rb
+++ backend/book-club-api/app/controllers/reviews_controller.rb
@@ -1,2 +1,40 @@
 class ReviewsController < ApplicationController
+  before_action :set_params, only: [:show, :update, :destroy]
+
+  def index
+    @reviews = Review.all
+    render json: @reviews, status: 200
+  end
+
+  def create
+    @review = Review.create(review_params)
+    render json: @review, status: 200
+  end
+
+  def show
+    render json: @review, status: 200
+  end
+
+  def update
+    @review.update(review_params)
+    if @review.save
+      render json: @review, status: 200
+    else
+      render json: { errors: @review.errors.full_messages }, status: 422
+    end
+  end
+
+  def destroy
+    @review.destroy
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:body, :reviewer, :book_id)
+  end
+
+  def set_params
+    @review = Review.find(params[:id])
+  end
 end
